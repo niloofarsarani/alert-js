622aec47669391c2bf3e69a17ac173563944a533
niloofarsarani <noloofarsarani8.gmail.com>
2023-07-19 18:36:40 +0330

    finished js

---
+++ index.js
@@ -0,0 +1,7 @@
+let userNumberOne = +prompt("Enter your first number")
+let userNumbertwo = +prompt("Enter your second number")
+
+let userNumberPlus = userNumberOne + userNumbertwo
+
+document.write (" the result is", userNumberPlus)
+console.log(userNumberPlus)
